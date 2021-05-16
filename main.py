f246d19870f6612dbc50d8f2db93d9ea5a0dd565
Dayana Sanchez <dayana.sanchezg@gmail.com>
2021-05-16 18:53:48 -0400

    practica c1e1

---
+++ main.py
@@ -0,0 +1,5 @@
+name = input("Ingrese su nombre ")
+edad = input("Ingrese su edad ")
+carrera = input("Ingrese su carrera ")
+
+print("Hola Mundo! mi nombre es " + name + " y tengo " + edad + " años, estudio " + carrera + " en la UNIMET")
